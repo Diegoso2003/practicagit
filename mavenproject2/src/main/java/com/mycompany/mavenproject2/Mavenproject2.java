3cb4802163a68f91821f095330f306fa40848b26
Diego <diegocayax202231074@cunoc.edu.gt>
2024-07-25 16:18:25 -0600

    agregando cosas

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author rafael-cayax
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
