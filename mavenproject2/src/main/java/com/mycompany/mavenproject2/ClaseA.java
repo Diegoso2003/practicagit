b2761bf3ef131b976253378451ae69229a5657ed
Diego <diegocayax202231074@cunoc.edu.gt>
2024-08-09 23:43:13 -0600

    clase nueva

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/ClaseA.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author rafael-cayax
+ */
+class ClaseA {
+    
+    private int numero;
+    private String nombre;
+    private int dpi;
+    
+}
